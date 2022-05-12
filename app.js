e4f71505f9afff2609926f690e9b81c916a723fd
carmen09 <44234186+carmen09@users.noreply.github.com>
2022-05-11 20:55:50 -0400

    pppp

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//初始化当前的app
+//Initialize the current app
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -7,6 +7,7 @@ var express =require('express');
 var path = require('path');
 
 //直接获取路由信息, 设置获取文档的位置
+////Get routing information directly, set the location to get the document
 app.use(express.static(path.join(__dirname, '/')));
 //通过获取get参数,定位发送文档. 如果没有任何参数信息,则直接发送home页面
 app.get('/', (req, res) => {
@@ -14,13 +15,17 @@ app.get('/', (req, res) => {
 });
 
 //socket连接并且开启监听
+////socket connection and start listening
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
 });
-//node监听当前http端口
+//node listens on the current http port
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
+
+//This is file informtion is most from the https://socket.io/get-started/chat
+//And I change it to fit my web
 
